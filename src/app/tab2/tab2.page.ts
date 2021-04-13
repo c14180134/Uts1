755b8c03d1e726ac41252273a47d6a42ff36832a
hartantio <c14180134@john.petra.ac.id>
2021-04-13 13:07:55 +0700

    bingung

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,13 +9,21 @@ import { Observable } from 'rxjs';
 })
 export class Tab2Page {
   isiData : Observable<data[]>;
+
+  Judul:string;
+  Isi:string;
+  Nilai:string;
   
   isiDataColl : AngularFirestoreCollection<data>;
   constructor(afs:AngularFirestore) {
     this.isiDataColl = afs.collection('dataNote');
     this.isiData = this.isiDataColl.valueChanges();
   }
-
+  kliked(ganti:string,si:string,Nil:string){
+    this.Judul=ganti
+    this.Isi=si
+    this.Nilai=Nil
+  }
   delete(i:string){
     console.log(i)
     
